8a49a38b4b7d443fcc43710ad64906d83debf827
JamesWClark <jwclark@rockhursths.edu>
2016-05-16 00:06:39 -0500

    cool - works in angular now

--- auth.js
+++ auth.js
@@ -1,59 +1,69 @@
-// https://developers.google.com/identity/sign-in/web/sign-in
-// https://developers.google.com/identity/sign-in/web/listeners
+var app = angular.module('ga',[]);
 
-var auth2;
+app.controller('gac', function($scope, $window) {
+    $scope.hi = 'hi';
+    
+    var auth2;
 
-var appStart = function() {
-    console.log('loading api');
-    gapi.load('auth2', initSigninV2);
-};
+    $window.appStart = function() {
+        console.log('loading api');
+        gapi.load('auth2', initSigninV2);
+    };
 
 
-var initSigninV2 = function() {
-    console.log('init signin v2');
-    auth2 = gapi.auth2.getAuthInstance();
-    auth2.isSignedIn.listen(signinChanged);
-    auth2.currentUser.listen(userChanged);
-};
+    var initSigninV2 = function() {
+        console.log('init signin v2');
+        auth2 = gapi.auth2.getAuthInstance();
+        auth2.isSignedIn.listen(signinChanged);
+        auth2.currentUser.listen(userChanged);
+    };
 
-var signinChanged = function(val) {
-    console.log('signin state changed to ', val);
-    if(val === false) {
+    var signinChanged = function(val) {
+        console.log('signin state changed to ', val);
+        if(val === false) {
+            updateHtml(false);
+        }
+    };
+
+    // this seems to fire right before the signout happens, as if to provide a reference to the user object on their way out of the application
+    var userChanged = function(googleUser) {
+        console.log('user changed: ', googleUser);
+        // if the page is refreshed and user is signed out, the script still generates a user with null fields
+        if(googleUser.El == null && googleUser.hg == null) {
+            updateHtml(false);
+        } else {
+            updateHtml(googleUser);
+        }
+    };
+
+    $scope.signOut = function() {
+        console.log('signing out');
+        auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut();
         updateHtml(false);
-    }
-};
-
-// this seems to fire right before the signout happens, as if to provide a reference to the user object on their way out of the application
-var userChanged = function(googleUser) {
-    console.log('user changed: ', googleUser);
-    // if the page is refreshed and user is signed out, the script still generates a user with null fields
-    if(googleUser.El == null && googleUser.hg == null) {
+    };
+    
+    $scope.disconnect = function() {
+        console.log('disconnecting');
+        auth2 = gapi.auth2.getAuthInstance().disconnect();
         updateHtml(false);
-    } else {
-        updateHtml(googleUser);
-    }
-};
+    };
 
-var signOut = function() {
-    console.log('signing out');
-    auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut();
-    updateHtml(false);
-};
+    var updateHtml = function(googleUser) {
+        console.log('update html: ', googleUser === false ? 'dashes' : googleUser);
+        if(googleUser) {
+            $('#curr-user-cell').text(JSON.stringify(googleUser, undefined, 2));
+            $('#signed-in-cell').text(auth2.isSignedIn.get());
+            $('#user-id').text(googleUser.getId());
+            $('#user-scopes').text(googleUser.getGrantedScopes());
+            $('#auth-response').text(JSON.stringify(googleUser.getAuthResponse(), undefined, 2));    
+        } else {
+            $('#curr-user-cell').text('--');
+            $('#signed-in-cell').text('--');
+            $('#user-id').text('--');
+            $('#user-scopes').text('--');
+            $('#auth-response').text('--');  
+        }
+    };
+});
 
-var updateHtml = function(googleUser) {
-    console.log('update html: ', googleUser === false ? 'dashes' : googleUser);
-    if(googleUser) {
-        $('#curr-user-cell').text(JSON.stringify(googleUser, undefined, 2));
-        $('#signed-in-cell').text(auth2.isSignedIn.get());
-        $('#user-id').text(googleUser.getId());
-        $('#user-scopes').text(googleUser.getGrantedScopes());
-        $('#auth-response').text(JSON.stringify(googleUser.getAuthResponse(), undefined, 2));    
-    } else {
-        $('#curr-user-cell').text('--');
-        $('#signed-in-cell').text('--');
-        $('#user-id').text('--');
-        $('#user-scopes').text('--');
-        $('#auth-response').text('--');  
-    }
-};
