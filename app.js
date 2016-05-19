29dbea9da16ede255190fb214ddd2081231271ae
J.W. Clark <jwclark@rockhursths.edu>
2016-05-18 21:13:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 
 app.use('/', express.static(__dirname + '/site'));
 
-http.listen(process.env.PORT || 80, function() {
+http.listen(80, function() {
     console.log('hosting from ' + __dirname);
     console.log('listening on 80');
 });
