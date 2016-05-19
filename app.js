bc63eeab50d8b00c630fbf0bbf57ac99742eef24
J.W. Clark <jwclark@rockhursths.edu>
2016-05-18 21:14:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use('/', express.static(__dirname + '/site'));
+app.use('/', express.static(__dirname + '/'));
 
 http.listen(80, function() {
     console.log('hosting from ' + __dirname);
